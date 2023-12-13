bd5820ad2058a2cbf4a9b106533f5f99b96320cd
gustavodessunte <gustavo.bernardes@sptech.school>
2023-12-13 14:00:07 -0300

    (CREATED) menu componente

--- ProjetoFutebol/src/app/app.module.ts
+++ ProjetoFutebol/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [
     provideClientHydration()
